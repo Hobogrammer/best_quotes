9aeab083aec899e603254ca6b8600b3d8ccc311e
Andre Smith <lordsilent@gmail.com>
2014-03-02 23:23:59 -0800

    Chapter 6

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -23,4 +23,19 @@ class QuotesController < Rulers::Controller
     m = FileModel.create attrs
     render :quote, :obj => m
   end
+
+  def update
+    if env["REQUEST_METHOD"] == "POST"
+      quote = Rulers::Model::FileModel.find(1)
+      quote["submitter"] = "John Wilks Booth"
+      m = FileModel.save quote
+     end
+      render :quote, :obj => quote
+  end
+
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render_response :quote, :obj => quote, :ua => ua
+  end
 end
